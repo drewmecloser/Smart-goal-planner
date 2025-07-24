f9013b88af14d5c7ff1a9c43c3be36f531b6f1b3
Andrew Omori <andrewomori7@gmail.com>
2025-07-24 12:15:47 +0300

    new update

--- Smart-goal-planner/src/components/GoalList.jsx
+++ Smart-goal-planner/src/components/GoalList.jsx
@@ -1,15 +1,20 @@
-import React from 'react'
-import GoalItem from './components/GoalItem';
+import React from 'react';
+import GoalCard from './GoalCard'; 
 
 function GoalList({ goals, onUpdateGoal, onDeleteGoal }) {
     return (
         <div className="goal-list">
-            <h2>Your Savings Goals</h2>
+            <h2>Your Savings Goals</h2> 
             {goals.map((goal) => (
-                <GoalItem key={goal.id} goal={goal} onUpdateGoal={onUpdateGoal} />
+                <GoalCard
+                    key={goal.id}
+                    goal={goal}
+                    onUpdateGoal={onUpdateGoal}
+                    onDeleteGoal={onDeleteGoal} 
+                />
             ))}
         </div>
-    )
+    );
 }
 
 export default GoalList;
